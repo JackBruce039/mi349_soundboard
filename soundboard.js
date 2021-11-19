f612a8ab36dc3790881fe6f4d2cd80451b7157a0
JackBruce039 <brucejac@msu.edu>
2021-11-18 23:59:00 -0500

    Soundboard

---
+++ soundboard.js
@@ -0,0 +1,17 @@
+var button1 = document.getElementById('but1');
+var audio3 = document.getElementById('audio1');
+button1.addEventListener("click", function() {
+    audio1.play();
+});
+
+var button2 = document.getElementById('but2');
+var audio2 = document.getElementById('audio2');
+button2.addEventListener("click", function() {
+    audio2.play();
+});
+
+var button3 = document.getElementById('but3');
+var audio3 = document.getElementById('audio3');
+button3.addEventListener("click", function() {
+    audio3.play();
+});
